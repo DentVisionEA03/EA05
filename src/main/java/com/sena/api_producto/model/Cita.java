8fbf249552e41a54f7d93cd9b665e849f6518ff7
Jeffry Arrieta <jeffryunad@gmail.com>
2026-03-30 09:22:19 -0500

    Creo la entidad cita en el modelo

---
+++ src/main/java/com/sena/api_producto/model/Cita.java
@@ -0,0 +1,70 @@
+package com.sena.api_producto.model;
+
+import com.sena.api_producto.model.enums.EstadoCita;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+public class Cita {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String paciente;
+    private String odontologo;
+
+    private LocalDate fecha;
+    private LocalTime hora;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoCita estado;
+
+    // GETTERS Y SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getOdontologo() {
+        return odontologo;
+    }
+
+    public void setOdontologo(String odontologo) {
+        this.odontologo = odontologo;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public EstadoCita getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCita estado) {
+        this.estado = estado;
+    }
+}
