1d25caebc965a910e50c95561a038b4368827576
Jeffry Arrieta <jeffryunad@gmail.com>
2026-03-31 11:59:51 -0500

    se crra el setId en cita para facikitar mapeo

--- src/main/java/com/sena/api_producto/model/Cita.java
+++ src/main/java/com/sena/api_producto/model/Cita.java
@@ -28,6 +28,10 @@ public class Cita {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getPaciente() {
         return paciente;
     }
